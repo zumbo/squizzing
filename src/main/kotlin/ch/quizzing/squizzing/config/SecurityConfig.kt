8c31f2f2e46f0c0304db40df39e4fc7429b5368f
isc-zum <marcel.zumstein@isc-ejpd.admin.ch>
2026-01-17 14:07:30 +0100

    Redirect to login page.

--- src/main/kotlin/ch/quizzing/squizzing/config/SecurityConfig.kt
+++ src/main/kotlin/ch/quizzing/squizzing/config/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +32,9 @@ class SecurityConfig {
             }
             .csrf { csrf ->
                 csrf.ignoringRequestMatchers("/auth/magic-link")
+            }
+            .exceptionHandling { exceptions ->
+                exceptions.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint("/auth/login"))
             }
 
         return http.build()
